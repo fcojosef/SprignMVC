c2da4d47f584e2ac50661deba3794210f65ab995
fcojosef <fcojosef.o@gmail.com>
2021-03-13 23:25:48 -0300

    Create project SpringMVC

---
+++ springMVC/src/test/java/com/guido/springMVC/SpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guido.springMVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
